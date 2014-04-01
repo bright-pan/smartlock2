e490a3523a0b51cdb525b81699673588c356dccc
wangzw <wangzw@yuettak.com>
2014-04-01 17:32:33 +0800

    add key add delete net message
    Signed-off-by: wangzw <wangzw@yuettak.com>

--- bsp/stm32f10x/applications/net/netmailclass.h
+++ bsp/stm32f10x/applications/net/netmailclass.h
@@ -8,6 +8,8 @@ rt_uint8_t msg_mail_opendoor(rt_uint8_t type,rt_uint16_t key,rt_uint32_t time);
 rt_uint8_t msg_mail_battery(rt_uint8_t status,rt_uint8_t capacity,rt_uint32_t time);
 rt_uint8_t msg_mail_adjust_time(void);
 rt_uint8_t msg_mail_alarmarg(rt_uint8_t Type,rt_uint8_t arg);
+rt_uint8_t msg_mail_addrkey(net_keyadd_user *KeyData);
+
 void msg_null_ack(message_type MSGType);
 
 
