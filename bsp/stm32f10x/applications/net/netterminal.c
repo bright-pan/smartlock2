b196e37ac94ab00578e3770692cf1341dfd5ecf4
wangzw <wangzw@yuettak.com>
2014-04-09 11:47:08 +0800

    create net  remote terminal controller process file
    Signed-off-by: wangzw <wangzw@yuettak.com>

--- bsp/stm32f10x/applications/net/netterminal.c
+++ bsp/stm32f10x/applications/net/netterminal.c
@@ -1,1 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    netphone.c
+  * @author  wangzw <wangzw@yuettak.com>
+  * @version v0.1
+  * @date    2014-4-2
+  * @brief   This file provides net message phone process functions.
+  ******************************************************************************
+  * @attention
+  *
+	*
+  ******************************************************************************
+  */
+#include "netterminal.h"
+
+
+/** 
+@brief modify SmartLock alarm arg
+@param mail: receive net message mail
+@retval RT_EOK	 :Successful operation
+@retval RT_ERROR :operation failure
+*/
+rt_err_t net_modify_alarm_arg(net_recvmsg_p mail)
+{
+	return RT_EOK;
+}
+
+/** 
+@brief modify SmartLock alarm arg
+@param mail: receive net message mail
+@retval RT_EOK	 :Successful operation
+@retval RT_ERROR :operation failure
+*/
+//rt_err_t net_m
+
 
--- bsp/stm32f10x/applications/net/netterminal.h
+++ bsp/stm32f10x/applications/net/netterminal.h
@@ -1,1 +1,6 @@
+#ifndef __NETTERMINAL_H__
+#define __NETTERMINAL_H__
+#include "rtthread.h"
+#include "netprotocol.h"
 
+#endif
