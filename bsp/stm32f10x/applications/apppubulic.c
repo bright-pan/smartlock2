561e6e3dde614aaf0f3c778d21d15dfc0989e096
wangzw <wangzw@yuettak.com>
2014-04-16 16:29:32 +0800

    add device need using of function

--- bsp/stm32f10x/applications/apppubulic.c
+++ bsp/stm32f10x/applications/apppubulic.c
@@ -1,5 +1,8 @@
 #include "apppubulic.h"
-
+#include "alarm.h"
+#include "local.h"
+#include "untils.h"
+#include "voice.h"
 
 /*
 @brief 求平均数
@@ -29,6 +32,182 @@ rt_uint16_t get_average_value(rt_uint16_t dat[],rt_uint8_t num)
 	return result;
 }
 
+/** 
+@brief  get new add key pos
+@param  void
+@retval spoce key pos
+*/
+rt_uint16_t get_new_key_pos(void)
+{
+	rt_uint16_t i;
+
+	for(i = 0 ;i < KEY_NUMBERS;i++)
+	{
+		if(device_config.param.key[i].flag == 0)
+		{
+			return i;
+		}
+	}
+
+	return KEY_NUMBERS;
+}
+
+/** 
+@brief  get fprint key number 
+@param  void
+@retval all fprint key number
+*/
+rt_uint16_t get_fprint_key_num(void)
+{
+  rt_uint16_t i;
+  rt_uint16_t num = 0;
+  
+	for(i = 0 ;i < KEY_NUMBERS;i++)
+	{
+	  if(device_config.param.key[i].flag == 1)
+	  {
+	  	if(device_config.param.key[i].key_type == KEY_TYPE_FPRINT)
+	  	{
+	  		rt_kprintf("this i = %d\n",i);
+        num++;
+	  	}
+	  }
+	}
+
+	return num;
+}
+
+/** 
+@brief  change key use status
+@param  key :key position
+@param	status :this position key new status
+@retval void 
+*/
+void set_key_using_status(rt_uint16_t key,KEY_TYPE type,rt_uint8_t status)
+{
+	if(key < KEY_NUMBERS)
+	{
+    device_config.param.key[key].flag = status;
+    device_config.param.key[key].key_type = type;
+	}
+	else
+	{
+		rt_kprintf("Key Pos Can,t > KEY_NUMBERS!!!\n");
+	}
+	device_config_file_operate(&device_config,1);
+}
+
+/** 
+@brief  change key use status
+@param  key :key position
+@param	status :this position key new status
+@retval void 
+*/
+rt_bool_t check_fprint_pos_inof(rt_uint16_t pos)
+{
+  rt_uint16_t i;
+  
+	for(i = 0 ;i < KEY_NUMBERS;i++)
+	{
+	  if(device_config.param.key[i].flag == 1)
+	  {
+	  	if(device_config.param.key[i].key_type == KEY_TYPE_FPRINT)
+	  	{
+        if(i == pos)
+        {
+					return RT_TRUE;
+        }
+	  	}
+	  }
+	}
+
+	rt_kprintf("Not find fprint\n\n");
+	return RT_FALSE;	
+}
+
+/*
+功能:操作网络协议中的各种事件
+参数:mode 模式  type 事件类型
+返回: -------------------------
+		 |模式 |成功|失败|功能    |
+		 |0    |0   |1   |发送事件|
+		 |1    |0   |1   |收到事件|
+		 |2    |0   |1   |清除事件|
+		 --------------------------
+*/
+static rt_event_t system_event = RT_NULL;
+rt_uint8_t system_event_process(rt_uint8_t mode,rt_uint32_t type)
+{
+	rt_uint32_t value;
+	rt_err_t    result;
+	rt_uint8_t  return_data = 1;
+
+	if(system_event == RT_NULL)
+	{
+		system_event = rt_event_create("system",RT_IPC_FLAG_FIFO);
+		RT_ASSERT(system_event != RT_NULL);
+	}
+	switch(mode)
+	{
+		case 0:	//set event 
+		{
+			result = rt_event_send(system_event,type);
+			if(result == RT_EOK)
+			{
+				return_data = 0;
+			}
+			break;
+		}
+		case 1:	//get event 
+		{
+			result = rt_event_recv(system_event,
+			                       type,
+			                       RT_EVENT_FLAG_OR,
+			                       RT_WAITING_NO,&value);
+			if(result == RT_EOK)
+			{
+				return_data = 0;
+			}
+			else if(result == -RT_ETIMEOUT)
+			{
+				return_data = 1;
+			}
+			break;
+		}
+		case 2://clean event
+		{
+			result = rt_event_recv(system_event,
+			                       type,
+			                       RT_EVENT_FLAG_OR | 
+			                       RT_EVENT_FLAG_CLEAR,
+			                       RT_WAITING_NO,&value);
+			if(result == RT_EOK)
+			{
+				return_data = 0;
+			}
+			break;
+		}
+    case 3://clean all event 
+    {
+      result = rt_event_recv(system_event,
+                             SYS_EVENT_ALL,
+                             RT_EVENT_FLAG_OR | 
+                             RT_EVENT_FLAG_CLEAR,
+                             RT_WAITING_NO,&value);
+      if(result == RT_EOK)
+      {
+        return_data = 0;
+      }
+      break;
+    }
+    default:
+    {
+			break;
+    }
+	}
+
+	return return_data;
+}
 
 /*
 功能:获得当前时间
--- bsp/stm32f10x/applications/apppubulic.h
+++ bsp/stm32f10x/applications/apppubulic.h
@@ -1,7 +1,25 @@
 #ifndef __APPPUBULIC_H__
 #define __APPPUBULIC_H__
 #include "rtthread.h"
+#include "untils.h"
+
+#define SYS_FPRINT_REGISTER  (0X01<<0)
+#define SYS_EVENT_ALL        0xffffffff
+
+
 rt_uint16_t get_average_value(rt_uint16_t dat[],rt_uint8_t num);
+
+rt_uint16_t get_new_key_pos(void);
+
+rt_uint16_t get_fprint_key_num(void);
+
+void set_key_using_status(rt_uint16_t key,KEY_TYPE type,rt_uint8_t status);
+
+rt_bool_t check_fprint_pos_inof(rt_uint16_t pos);
+
+rt_uint32_t sys_cur_date(void);
+
+rt_uint8_t system_event_process(rt_uint8_t mode,rt_uint32_t type);
 
 #endif
 
