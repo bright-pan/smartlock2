5e4f6488356175850e86785f446a0b4019136644
wangzw <wangzw@yuettak.com>
2014-12-01 10:10:11 +0800

    添加指纹头电源宏定义

--- bsp/stm32f10x/rtconfig.h
+++ bsp/stm32f10x/rtconfig.h
@@ -196,5 +196,5 @@
 #define USEING_RAM_DEBUG      	//使用动态调试信息打印功能
 #define USEING_GSM_UART_MANAGE 	//使用gsm串口硬件管理
 #define USEING_LOW_POWER				//使用低功耗处理
-
+//#define NOT_CLOSE_FT_POWR       //指纹头不关闭电源
 #endif
